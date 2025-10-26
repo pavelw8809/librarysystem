483b28c3d10c5ad407ddbd26ad7a0b04e41a68b6
Wieczorek <pwieczorek@luxoft.com>
2025-10-26 17:09:26 +0100

    LibrarySystem new

--- LibrarySystem/obj/Debug/net8.0/LibrarySystem.AssemblyInfo.cs
+++ LibrarySystem/obj/Debug/net8.0/LibrarySystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibrarySystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe85e2269bbcc4403234456bfbcb3e3cb82a1661")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibrarySystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibrarySystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
